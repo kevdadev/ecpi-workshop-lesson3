8c9cbf5f1cfd62e655497a074783cb8441116383
Kevin Israel <kevdadev@gmail.com>
2019-05-03 21:02:33 -0400

    initial commit

---
+++ intro2VSCode/scripts/index.js
@@ -0,0 +1,6 @@
+function ShowResult()
+{
+    var e = document.getElementById("lblResult");
+    e.innerHTML = "This is how javascript can modify the DOM dynamically";
+    e.style.backgroundColor = "#ff0000";
+}
